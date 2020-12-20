a6cd2ce23188dbc9bb7ae69036b28b1c74561cc1
abrookes <abrookes@mines.edu>
2020-12-20 00:29:12 -0500

    co

--- soup.py
+++ soup.py
@@ -3,32 +3,48 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import json
+import os.path
 
-good_color = 'black'
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
 URL = 'https://www.freitag.ch/en/f41'
+found = False
 
-# driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
+driver = webdriver.Chrome(config['driver-path'], )
 
-driver = webdriver.Chrome('/Users/aidanbrookes/Downloads/chromedriver', )
 driver.get(URL)
+time.sleep(0.5)
 driver.find_element_by_id('dismiss-cookies-cta').click()
 time.sleep(0.5)
-driver.find_element_by_id('products-load-all').click()
-time.sleep(0.5)
 
-browse = driver.find_element_by_id('products-selector')
-items = browse.find_elements_by_tag_name('li')
+while not found:
+    time.sleep(1)
+    driver.refresh()
 
-for item in items:
-    color = item.get_attribute('data-dimension17')
-    if good_color in color:
-        url = item.find_element_by_tag_name('a').get_attribute('href')
-        driver.get(url)
-        driver.find_element_by_id('product-addtocart').click()
-        # driver.execute_script(f"window.open('{url}');")
-        break
+    try:
+        driver.find_element_by_id('products-load-all').click()
+    except:
+        pass
+    time.sleep(0.1)
 
-time.sleep(0.5)
+    browse = driver.find_element_by_id('products-selector')
+    items = browse.find_elements_by_tag_name('li')
+
+    for item in items:
+        color = item.get_attribute('data-dimension17')
+        if config['color'] in color:
+            found = True
+            url = item.find_element_by_tag_name('a').get_attribute('href')
+            driver.get(url)
+            time.sleep(1)
+            driver.find_element_by_id('product-addtocart').click()
+            time.sleep(2)
+            driver.execute_script(f"window.open('{config['alarm-url']}');")
+            break
+
+
 # driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
 # print(drivera.find_element_by_tag_name('body'))
 # print(len(items))
