157a4245286d6da7cd8dd5504239d6b6a9b82815
abrookes <abrookes@mines.edu>
2020-12-21 21:33:18 -0500

    a

--- soup.py
+++ soup.py
@@ -1,21 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 import time
 import json
-import os.path
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 URL = 'https://www.freitag.ch/en/f41'
-found = False
-
-driver = webdriver.Chrome(config['driver-path'], )
+checkoutURL = 'https://www.freitag.ch/en/checkout/'
 
 
 def main():
+    found = False
+
+    driver = webdriver.Chrome(config['driver-path'], )
     driver.get(URL)
     time.sleep(0.5)
     driver.find_element_by_id('dismiss-cookies-cta').click()
@@ -23,14 +23,19 @@ def main():
 
     while not found:
         try:
-            check_stock()
+            found = check_stock(driver)
         except:
             pass
+
         driver.refresh()
+        if 'FREITAG' not in driver.find_element_by_tag_name('title').get_attribute('innerHTML'):
+            # assume 404
+            driver.execute_script(f"window.open('{config['alarm-url']}');")
+
         time.sleep(config['wait-seconds'])
 
 
-def check_stock():
+def check_stock(driver):
     driver.find_element_by_id('products-load-all').click()
     time.sleep(0.1)
 
@@ -40,14 +45,21 @@ def check_stock():
     for item in items:
         color = item.get_attribute('data-dimension17')
         if config['color'] in color:
-            found = True
             url = item.find_element_by_tag_name('a').get_attribute('href')
             driver.get(url)
             time.sleep(1)
             driver.find_element_by_id('product-addtocart').click()
-            time.sleep(2)
+            time.sleep(1)
             driver.execute_script(f"window.open('{config['alarm-url']}');")
-            break
+            checkout(driver)
+            return True
+
+    return False
+
+
+def checkout(driver):
+    driver.get(checkoutURL)
+
 
 
 if __name__ == "__main__":
